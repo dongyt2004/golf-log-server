26373aacd4e89a7a0c025513a42d5697ddcb8bc9
dongyt <dongyt2004@163.com>
2020-03-10 21:51:21 +0800

    1st commit

---
+++ logserver.js
@@ -0,0 +1,25 @@
+process.env.TZ = "Asia/Shanghai";
+const log4js = require('log4js');
+const cluster = require('cluster');
+const numCPUs = require('os').cpus().length;
+/** ---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------- **/
+if (cluster.isMaster) {
+    log4js.configure({
+        appenders: {
+            file: {type: 'dateFile', filename: 'server.log', daysToKeep: 7, keepFileExt: true,
+                layout: {
+                    type: 'pattern',
+                    pattern: '%d{yyyy-MM-dd hh:mm:ss} [%p] %h %X{real_name} - %m'
+                }
+            },
+            server: {type: 'tcp-server', host: '0.0.0.0', port: 5000}
+        },
+        categories: {
+            default: {appenders: ['file'], level: 'debug'}
+        }
+    });
+
+    for (i = 0; i < numCPUs; i++) {
+        cluster.fork();
+    }
+}
